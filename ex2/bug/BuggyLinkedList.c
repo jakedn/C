a9801ff98280b72e1c0f9421ed4d97d86b2a7d67
yaako_000 <yaakovn@live.com>
2017-11-23 14:27:03 +0200

    bug fixed

--- ex2/bug/BuggyLinkedList.c
+++ ex2/bug/BuggyLinkedList.c
@@ -9,7 +9,7 @@ struct node
     nodeStruct *next;
 };
 
-nodeStruct *head; //head wasnt defined properly
+nodeStruct *head;   //head wasnt defined properly
 int sizeOfNodes;
 
 /**
@@ -18,18 +18,20 @@ int sizeOfNodes;
  */
 void addToEnd(int dataToAdd)
 {
-    nodeStruct *temp,*right;
-    temp= (struct node *)malloc(sizeof(struct node));
-    temp->data = (int*) malloc(sizeof(int));    //cast to int and not int*
-    temp->data=dataToAdd;
-    right=(struct node *)head;
+    nodeStruct *temp, *right;
+    temp = (struct node *)malloc(sizeof(struct node));
+    //temp->data = (int) malloc(sizeof(int));
+    //the above line was taken out because memory is already allocated in nodeStruct
+    //for the data no need for another malloc that leads to memory leakage
+    temp->data = dataToAdd;
+    right = (struct node *)head;
 
     while(right->next != NULL)
-        right=right->next;
+        right = right->next;
 
-    right->next =temp;
-    right=temp;
-    right->next=NULL;
+    right->next = temp;
+    right = temp;
+    right->next = NULL;
 }
 
 /**
@@ -39,18 +41,21 @@ void addToEnd(int dataToAdd)
 void addFirst(int dataToAdd)
 {
     nodeStruct *tempNode;
-    tempNode=(struct node *)malloc(sizeof(struct node));
-    tempNode->data = (int*) malloc(sizeof(int));     //casted int not int*
+    tempNode = (struct node *)malloc(sizeof(struct node));
+    //tempNode->data = (int) malloc(sizeof(int));
+    //the above line was taken out because memory is already allocated in nodeStruct
+    //for the data no need for another malloc that leads to memory leakage
     tempNode->data = dataToAdd;
-    if (head == NULL)   //codestyle correction
-    {     //code style correction
+    if (head == NULL)
+    {
         head = tempNode;
         head->next = NULL;
     }
     else
-    {   //codestyle correction
-        //we need to save the old heads address first otherwise we lose the chain
-        void *temp = head;
+    {
+        //we need to save the old heads address first otherwise we lose the chain of the linked list
+        //the way it was before is we had tempnode==head and tempnode->next==tempnode
+        nodeStruct *temp = head;
         head = tempNode;
         tempNode->next = temp;
     }
@@ -65,19 +70,26 @@ void addFirst(int dataToAdd)
 void addInPlace(int dataToAdd, int placeToAdd)
 {
     int i;
-    nodeStruct *temp,*left,*right;
-    right=head;
-    for(i=0;i<placeToAdd+1;i++)
-        left = right;
-    right = right->next;
+    nodeStruct *temp, *left, *right;
+    right = head;
 
-    temp=(struct node *)malloc(sizeof(struct node));
-    temp->data = (int) malloc(sizeof(int));
-    temp->data=dataToAdd;
-    left->next=temp;
-    left=temp;
-    left->next=right;
-    return;
+    // the for loops contents only had the left=right line in it so it did that place to add timesso i fixed the
+    // loop contents also there was a logic error placeTOAdd is index of the new node that needs to be added
+    // so we need to skip exacly placeToAdd nodes from the begginning not placeToAdd+1 nodes like was written
+    for(i = 0; i < placeToAdd; i++)
+    {
+        left = right;
+        right = right->next;
+    }
+    temp = (struct node *)malloc(sizeof(struct node));
+    //temp->data = (int) malloc(sizeof(int));
+    //the above line was taken out because memory is already allocated in nodeStruct
+    //for the data no need for another malloc that leads to memory leakage
+    temp->data = dataToAdd;
+    left->next = temp;
+    left = temp;
+    left->next = right;
+    return;//TODO
 }
 
 
@@ -87,41 +99,51 @@ void addInPlace(int dataToAdd, int placeToAdd)
  * @param dataToAdd the data to insert
  * @param numberOfNodes the current number of nodes
  */
-void insert(int dataToAdd,int *numberOfNodes)
+void insert(int dataToAdd, int *numberOfNodes)
 {
     int c = 0;
     nodeStruct *temp;
-    temp=head;
-    if(temp==NULL)
+    temp = head;
+    if(temp == NULL)
     {
         addFirst(dataToAdd);
     }
     else
     {
-        while(temp!=NULL)
+        //the next loop went into an infinite loop without the correction made (stated below)
+        //because we can get to a temp != null can the if condition wont trigger
+        while(temp != NULL)
         {
-            if(temp->data<dataToAdd)
+            if(temp->data < dataToAdd)
             {
                 c++;
-                temp = temp->next;
             }
-
+            //CORRECTION: i moved the line below outside the if statement so we continue down the chain
+            //regardless of the if statement and the loop will terminate eventually
+            temp = temp->next;
         }
 
-        if(c==0) {
+        if(c==0)
+        {
             addFirst(dataToAdd);
         }
 
-        else if(c<*numberOfNodes){
-            addInPlace(dataToAdd, ++c);
+        else if(c < *numberOfNodes)
+        {
+            //c represents the index of the linked list (starting from 0) of where data needs to be put
+            //this is because it is the amount of nodes we skipped from the beginning
+            //so i took out the ++
+            addInPlace(dataToAdd, c);
         }
         else
             addToEnd(dataToAdd);
     }
-    (*numberOfNodes)++;    //we need to add one from what numberofnodes points to not numberofnodes itself
+    // we need to add one from what numberofnodes points to not numberofnodes itself i added a star
+    // parentesis added to add 1 to what is pointer too and not the pointer
+    (*numberOfNodes)++;
 }
 
-//didnt have return comment so i added.
+// didnt have return comment so i added. and also added return values
 /**
  * deletes the first node with the number num
  * @param dataToDelete the data to delete
@@ -140,19 +162,19 @@ int deleteNode(int dataToDelete,int *numberOfNodes)
             if(temp == head)
             {
                 head=temp->next;
-                free(temp->data);   //making sure there are no leaks
                 free(temp);
 
             }
             else
             {
                 prev->next = temp->next;
-                free(temp->data);   //making sure there are no leaks
                 free(temp);
             }
-            (*numberOfNodes)--;     //we need to subtract one from what numberofnodes points to not numberofnodes itself
+            //we need to subtract one from what numberofnodes points to not numberofnodes itself i added a star
+            //added peretesis also to make sure it subtracts from the value pointer to and not the pointer
+            (*numberOfNodes)--;
+            //delete successful so i added the return value line below
             return 1;
-
         }
         else
         {
@@ -160,7 +182,7 @@ int deleteNode(int dataToDelete,int *numberOfNodes)
             temp = temp->next;
         }
     }
-
+    //if we got here we didnt delete anything so i return 0
     return 0;
 }
 
@@ -177,14 +199,12 @@ void  display(nodeStruct *startingNodeToPrint)
     {
         return;
     }
-
-    //here we print the last node as null this is a mistake because we want only the nodes with data
-    while(startingNodeToPrint->next!=NULL)
+    while(startingNodeToPrint!=NULL)
     {
         printf("%d --> ",startingNodeToPrint->data);
         startingNodeToPrint=startingNodeToPrint->next;
     }
-    printf("%d\n", startingNodeToPrint->data);
+    printf("NULL\n");
 }
 
 
@@ -231,11 +251,11 @@ void hardTest(){
 
     display(head);
     for (int i=0;i<100;i++){
-        delete(i,&sizeOfNodes);
+        deleteNode(i,&sizeOfNodes);
     }
     display(head);
     for (int i=0;i<100;i++){
-        delete(i,&sizeOfNodes);
+        deleteNode(i,&sizeOfNodes);
     }
     display(head);
 }
